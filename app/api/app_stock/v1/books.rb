05aeb1036202dee9cf2e48d72147a88e0667c408
Clement Morisset <morissetcl87@gmail.com>
2017-04-19 15:45:59 +0200

    add new flow to api works

--- app/api/app_stock/v1/books.rb
+++ app/api/app_stock/v1/books.rb
@@ -5,17 +5,14 @@ module AppStock
       format :json
       prefix :api
 
-
       resource :books do
         desc "Return list of books"
-        # Récupération de la collection de toutes les voitures grâce à ActiveRecord
         get do
           books = Book.all
           present books
         end
 
         desc "Return a book"
-        # Récupération d'une voiture spécifique grâce au paramètre passé dans l'url
         params do
           requires :id, type: Integer, desc: "Book id"
         end
@@ -24,23 +21,28 @@ module AppStock
             book = Book.find(params[:id])
             present book, with: AppStock::Entities::Book
           end
+
+          resource :flows do
+            desc 'Create a flow.'
+            params do
+              requires :flow, type: Hash do
+                requires :newQuantity, type: Integer, desc: 'New Stock.'
+                requires :oldQuantity, type: Integer, desc: 'Previous Stock.'
+              end
+            end
+            post do
+              @book = Book.find(params[:id])
+              @flow = Flow.new(params[:flow])
+              if @flow = @book.flows.create!(params[:flow])
+                 @book.update(quantity: @flow.newQuantity)
+              end
+              puts params.inspect
+            end
+          end
         end
+
+
       end
-
-      # desc "update a book author"
-      # params do
-      #   requires :id, type: Integer, desc: "Book Id"
-      # end
-      # route_param :id do
-      #   patch do
-      #   Book.find(params[:id]).update!({
-      #     quantity:params[:quantity]
-      #   })
-      #   present book
-      #   end
-      # end
-
-
     end
   end
 end
